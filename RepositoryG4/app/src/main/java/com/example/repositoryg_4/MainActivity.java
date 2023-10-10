36a25d94b4ec83ac310cd51d53e70eadfc32013a
Denis Nazarovs <denis.nazarovs@va.lv>
2023-10-10 18:10:09 +0300

    Themes work on Activity 1

--- RepositoryG4/app/src/main/java/com/example/repositoryg_4/MainActivity.java
+++ RepositoryG4/app/src/main/java/com/example/repositoryg_4/MainActivity.java
@@ -19,28 +19,41 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // Get the selected theme from SharedPreferences or use the default theme
+        SharedPreferences sharedPref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        String selectedTheme = sharedPref.getString("selected_theme", "Default");
+
+        // Set the app's theme based on the selected theme, ignoring system settings ("Default" dropdown option does follow system settings)
+        if (selectedTheme.equals("Light theme")) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        } else if (selectedTheme.equals("Dark theme")) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
+        }
+
         setContentView(R.layout.activity_main);
 
-        SharedPreferences sharedPref = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
+        //Get the previously input text by user, but it into the input field
         EditText textInputField = findViewById(R.id.textInputEditText);
         String savedText = sharedPref.getString("user_input", "");
         textInputField.setText(savedText);
 
-        Spinner spinner = (Spinner) findViewById(R.id.themesSpinner);
-// Create an ArrayAdapter using the string array and a default spinner layout.
+        //Get dropdown, buttons
+        Spinner spinner = findViewById(R.id.themesSpinner);
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this,
                 R.array.themes,
                 android.R.layout.simple_spinner_item
         );
-// Specify the layout to use when the list of choices appears.
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner.
         spinner.setAdapter(adapter);
 
         Button act2Btn = findViewById(R.id.act2Button);
 
-        //Logic for button to go to second activity
+        //Create event listener hat takes user to MainActivity2
         act2Btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -48,40 +61,38 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        //Logic for button "Save"
         Button saveBtn = findViewById(R.id.saveButton);
 
+        //Create event listener that saves the input text field's text into Shared Preferences
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String userInput = String.valueOf(textInputField.getText());
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putString("user_input", userInput);
-        editor.apply();
-
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("user_input", userInput);
+                editor.apply();
             }
         });
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM); // Follow the system's theme initially
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM); // Follow the system's theme initially
-
+        //Store the selected dropdown item in Shared Preferences, immediately set the theme without needing to re-render the Activity
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parentView, View selectedItemView, int position, long id) {
                 String selectedTheme = spinner.getSelectedItem().toString();
+
+                // Save the selected theme in SharedPreferences
+                SharedPreferences.Editor editor = sharedPref.edit();
+                editor.putString("selected_theme", selectedTheme);
+                editor.apply();
+
+                // Change the app's theme, ignoring system settings
                 if (selectedTheme.equals("Dark theme")) {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES); // Set dark theme
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                } else if (selectedTheme.equals("Light theme")) {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
                 } else {
-                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM); // Set light theme
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM);
                 }
-                // Reapply the adapter to the spinner
-                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
-                        MainActivity.this,
-                        R.array.themes,
-                        android.R.layout.simple_spinner_item
-                );
-                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-                spinner.setAdapter(adapter);
             }
 
             @Override
@@ -89,7 +100,5 @@ public class MainActivity extends AppCompatActivity {
                 // Do nothing if nothing is selected
             }
         });
-
-
     }
 }
